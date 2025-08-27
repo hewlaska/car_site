4599f7e17d13838a3af9dda2336a07f0d85add21
hewlaska <kuznetsovadaria2002@gmail.com>
2025-08-27 17:40:07 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+});
